64a9e6ef802956235de7733efcc70382591f3c9e
AsmaaAlbaik <asmaa.baik@gmail.com>
2025-02-01 16:03:25 +0300

    update home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import PageContent from "../components/PageContent";
 
 export default function HomePage() {
-    return <PageContent title={'Home Page'}></PageContent>;
+    return <PageContent title={'Home Page'}>
+        <p>Welcome to the home page!</p>
+    </PageContent>;
 }
